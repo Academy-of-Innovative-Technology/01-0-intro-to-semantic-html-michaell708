52a9de51c4da5a96d164a74091411aada24cfd5f
michaell708 <michaell708@nycstudents.net>
2025-11-24 12:36:58 -0500

    done

--- script.js
+++ script.js
@@ -1,24 +1,31 @@
 var main = document.querySelector("main")
 let posts = [
    {
-    image: "",
-    title:"",
-    post:""
+    image: "http://placehold.it/200x200",
+    title:"blog1",
+    post:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur consequat suscipit sem id tincidunt. Quisque pulvinar est vitae turpis sodales, quis finibus ligula eleifend. Nam velit libero, tincidunt nec ex ac, interdum congue lacus. Phasellus sit amet scelerisque erat. Pellentesque at tincidunt metus, vel consequat mauris."
    },
    {
-    image:"",
-    title:"",
-    post:""
+    image:"http://placehold.it/200x200",
+    title:"blog2",
+    post:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur consequat suscipit sem id tincidunt. Quisque pulvinar est vitae turpis sodales, quis finibus ligula eleifend. Nam velit libero, tincidunt nec ex ac, interdum congue lacus. Phasellus sit amet scelerisque erat. Pellentesque at tincidunt metus, vel consequat mauris."
    },
    {
-    image:"",
-    title:"",
-    post:""
+    image:"http://placehold.it/200x200",
+    title:"blog3",
+    post:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur consequat suscipit sem id tincidunt. Quisque pulvinar est vitae turpis sodales, quis finibus ligula eleifend. Nam velit libero, tincidunt nec ex ac, interdum congue lacus. Phasellus sit amet scelerisque erat. Pellentesque at tincidunt metus, vel consequat mauris."
    }
 ];
 
 posts.forEach((post) => {
-    image.insertAdjacentHTML('afterend', '<img src="http://placehold.it/200x200"></img>');
-    title.insertAdjacentHTML('afterend', "<h1>blog</h1>");
-    post.insertAdjacentHTML('afterend', "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur consequat suscipit sem id tincidunt. Quisque pulvinar est vitae turpis sodales, quis finibus ligula eleifend. Nam velit libero, tincidunt nec ex ac, interdum congue lacus. Phasellus sit amet scelerisque erat. Pellentesque at tincidunt metus, vel consequat mauris.</p>");
+
+    var html = `
+        <img src="${ post.image }">
+        <h1>${ post.title }</h1>
+        <p>${ post.post }</p>
+    `;
+
+
+    main.insertAdjacentHTML('afterend', html);
+
 });
